d0d370ed4f7fe1dd18b917575bbd7d1f1df158c7
Zee Lavenhouse <Zeehlavenhouse@gmail.com>
2023-03-01 16:38:02 -0500

    Final commit after review

--- calculator.py
+++ calculator.py
@@ -10,56 +10,73 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 while True:
 
     #     read input
-    equation = input("Enter your equation:")
+    try:
+        equation = input("Enter your equation:")
 
-    #     tokenize input
+        #     tokenize input
 
-    tokens = equation.split(" ")
-    if "q" in tokens[0][0]:
-        print("you are exiting!")
-        break
-        
-    else:
-
-        #             (decide which math function to call based on first token)
-        #             if the first token is 'pow':
-        #                   call the power function with the other two tokens
-
-        #             (...etc.)
-
-        num1 = int(tokens[1])
-
-        if len(tokens) > 2:
-
-            num2 = int(tokens[2])
-
-        if tokens[0] == "+":
-            ans = add(num1,num2)
-    
-        elif tokens[0] == "-":
-            ans = subtract(num1, num2)
-    
-        elif tokens[0] == "*":
-            ans = multiply(num1, num2)
-        
-        elif tokens[0] == "/":
-            ans = divide(num1,num2)
-    
-        elif tokens[0] == "square":
-            ans = square(num1)
+        tokens = equation.split(" ")
+        if "q" in tokens[0][0]:
+            print("you are exiting!")
+            break
             
-        elif tokens[0] == "cube":
-            ans = cube(num1)
-
-        elif tokens[0] == "pow":
-            ans = pow(num1,num2)
-        
-        elif tokens[0] == "mod":
-            ans = mod(num1,num2)
-        
         else:
-            print("Please input proper format!")
 
-            
-    print(ans)
+            #             (decide which math function to call based on first token)
+            #             if the first token is 'pow':
+            #                   call the power function with the other two tokens
 
+            #             (...etc.)
+
+            ans = None
+
+            num1 = int(tokens[1])
+
+            if len(tokens) > 2:
+
+                num2 = int(tokens[2])
+            else:
+                num2 = 0
+
+            if tokens[0] in {'square','cube'}:
+
+                if tokens[0] == "square":
+                    ans = square(num1)
+                        
+                elif tokens[0] == "cube":
+                    ans = cube(num1)
+
+
+            elif tokens[0] in {'+','-','/','*','pow','mod'}:
+
+                if len(tokens) < 3:
+                    print("Insufficient arguments!")
+
+                else:
+
+                    if tokens[0] == "+":
+                        ans = add(num1,num2)
+                
+                    elif tokens[0] == "-":
+                        ans = subtract(num1, num2)
+                
+                    elif tokens[0] == "*":
+                        ans = multiply(num1, num2)
+                    
+                    elif tokens[0] == "/":
+                        ans = divide(num1,num2)
+
+                    elif tokens[0] == "pow":
+                        ans = pow(num1,num2)
+                    
+                    elif tokens[0] == "mod":
+                        ans = mod(num1,num2)
+                    
+                    else:
+                        print("Please input proper format!")
+
+                
+        print(ans)
+
+    except:
+        print("Please input an operator followed by numbers")
